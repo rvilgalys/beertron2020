4bb4104c3c70f3e4df35a860597f9f52d24485ae
Rim Vilgalys <vilgalys@gmail.com>
2019-01-10 11:57:51 -0500

    ....

--- app.js
+++ app.js
@@ -264,10 +264,10 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, "./frontend/build")));
+app.use(express.static(path.join(__dirname, "./frontend/build/")));
 
 app.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "./frontend/build", "index.html"));
+  res.sendFile(path.join(__dirname, "./frontend/build/", "index.html"));
 });
 
 mongoose
