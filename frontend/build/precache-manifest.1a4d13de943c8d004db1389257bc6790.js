b6efcf40b484976755e788eb9d793ad088604d06
Rim Vilgalys <vilgalys@gmail.com>
2019-01-10 17:11:06 -0500

    build missing?

---
+++ frontend/build/precache-manifest.1a4d13de943c8d004db1389257bc6790.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1b6a0e8dc7ca39f1032b",
+    "url": "/static/js/main.1b6a0e8d.chunk.js"
+  },
+  {
+    "revision": "e1168fbabe224b78d9ec",
+    "url": "/static/js/1.e1168fba.chunk.js"
+  },
+  {
+    "revision": "1b6a0e8dc7ca39f1032b",
+    "url": "/static/css/main.f37dd6e2.chunk.css"
+  },
+  {
+    "revision": "cc3fb8bddc556f23b4adcf8765f659a3",
+    "url": "/index.html"
+  }
+];
